d7a7653b3e789c5cea6ac55439490ce1b49fe774
ruan-luqingnian-r <15991875018@163.com>
2021-10-31 21:25:12 +0800

    'test'

--- test/src/java1031/Sort.java
+++ test/src/java1031/Sort.java
@@ -1,5 +1,7 @@
 package java1031;
 
+import java.util.Arrays;
+
 /**
  * @Author: ruan
  * Date: 2021/10/31 20:59
@@ -7,6 +9,39 @@ package java1031;
  */
 public class Sort {
     public static void main(String[] args) {
+        int[] arr = new int[20];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * 100 + 1);
+        }
+        System.out.println("排序前：" + Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("排序后：" + Arrays.toString(arr));
+    }
+
+    /**
+     * 冒泡
+     * @param arr
+     */
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]){
+                    arr[j] = arr[j] ^ arr[j + 1];
+                    arr[j + 1] = arr[j + 1] ^ arr[j];
+                    arr[j] = arr[j] ^ arr[j + 1];
+                }
+            }
+        }
+    }
+
+    /**
+     * 选择排序
+     * @param arr
+     */
+    public static void selectSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+
+        }
 
     }
 }
