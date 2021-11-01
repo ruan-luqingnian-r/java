6193c391c6c4731506c2fb0fc938801930f6c8ab
ruan-luqingnian-r <15991875018@163.com>
2021-11-01 22:05:17 +0800

    'test'

--- test/src/java1031/MyHashMap.java
+++ test/src/java1031/MyHashMap.java
@@ -45,10 +45,90 @@ public class MyHashMap<K, V> {
                 temp = temp.next;
             }
             //此时，找到链表尾部
+            temp.next = newNode;
+            newNode.pre = temp;
         }
-
+        size++;
     }
 
+    /**
+     * 根据节点删除数据
+     * @param key
+     * @return
+     */
+    public V remove(K key){
+        //获取该节点
+        Node<K, V> node = getNodeFromKey(key);
+        if (node == null){
+            return null;
+        }
+        //判断该节点状态
+        if (node.next == null && node.pre == null){
+            //上下都为空，只有该节点一个，置空节点
+            table[hash(key)] = null;
+        }else if (node.next == null){
+            //节点下一个为空，说明是最后一个节点
+            node.pre.next = null;
+        }else if (node.pre == null){
+            //节点上一个为空，说明第一个节点
+            node.next.pre = null;
+            table[hash(key)] = node.next;
+        }else {
+            //中间节点
+            node.pre.next = node.next;
+            node.next.pre = node.pre;
+        }
+        return node.value;
+    }
+
+    /**
+     * 修改
+     * @param key
+     * @param value
+     * @return
+     */
+    public V set(K key, V value){
+        Node<K, V> node = getNodeFromKey(key);
+        if (node == null){
+            return null;
+        }
+        V v = node.value;
+        node.value = value;
+        return v;
+    }
+
+    public V get(K key){
+        Node<K, V> node = getNodeFromKey(key);
+        if (node == null){
+            return null;
+        }
+        return node.value;
+    }
+
+
+    /**
+     * 根据key获取节点
+     * @param key
+     * @return
+     */
+    private Node<K, V> getNodeFromKey(K key){
+        //获取hash值
+        int index = hash(key);
+        Node<K, V> node = table[index];
+        if (node == null){
+            return null;
+        }else {
+            Node<K, V> temp = node;
+            while (node.next != null){
+                if (node.key.equals(key)){
+                    return node;
+                }
+                node = node.next;
+            }
+        }
+        return null;
+
+    }
 
     private int hash(K key){
         return key == null ? 0 : key.hashCode() & (table.length - 1);
