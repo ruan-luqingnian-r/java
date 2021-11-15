e8813ae0c874a16ccc0fabc48e6b44596026b974
ruan-luqingnian-r <15991875018@163.com>
2021-11-15 20:36:41 +0800

    '算法练习'

--- test/src/java1102/Sort.java
+++ test/src/java1102/Sort.java
@@ -1,10 +1,125 @@
 package java1102;
 
+import java.util.Arrays;
+
 /**
  * @Author: ruan
  * Date: 2021/11/2 13:54
  * @Description:
  */
 public class Sort {
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100 + 1);
+        }
+        System.out.println("排序前：" + Arrays.toString(arr));
+        quickSort(arr);
+        System.out.println("排序后：" + Arrays.toString(arr));
+    }
+
+    /**
+     * 冒泡排序
+     */
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]){
+                    arr[j] = arr[j] ^ arr[j + 1];
+                    arr[j + 1] = arr[j + 1] ^ arr[j];
+                    arr[j] = arr[j] ^ arr[j + 1];
+                }
+            }
+        }
+    }
+
+    /**
+     * 选择排序
+     */
+    public static void selectSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            int minvalue = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < minvalue){
+                    minIndex = j;
+                    minvalue = arr[j];
+                }
+            }
+            if (i != minIndex){
+                arr[minIndex] = arr[i];
+                arr[i] = minvalue;
+            }
+        }
+    }
+
+    /**
+     * 插入排序
+     */
+    public static void insertSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int index = i - 1;
+            int value = arr[i];
+            while (index >= 0 && arr[index] > value ){
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = value;
+        }
+    }
+
+    public static void shellSort(int[] arr){
+        for (int gap = (arr.length / 2); gap > 0; gap /= 2){
+            for (int i = gap; i < arr.length; i++) {
+                int index = i - gap;
+                int value = arr[i];
+                while (index >= 0 && arr[index] > value){
+                    arr[index + gap] = arr[index];
+                    index = index - gap;
+                }
+                arr[index + gap] = value;
+            }
+        }
+    }
+
+    public static void quickSort(int[] arr){
+        quick(arr,0, arr.length - 1);
+    }
+
+    private static void quick(int[] arr, int left, int right){
+        int l = left;
+        int r = right;
+        int mod = arr[(left + right) / 2];
+        while (l < r){
+            while (arr[l] < mod){
+                l++;
+            }
+            while (arr[r] > mod){
+                r--;
+            }
+            if (l >= r){
+                break;
+            }
+            arr[l] = arr[l] ^ arr[r];
+            arr[r] = arr[r] ^ arr[l];
+            arr[l] = arr[l] ^ arr[r];
+            if (arr[l] == mod){
+                r--;
+            }
+            if (arr[r] == mod){
+                l++;
+            }
+        }
+        if (l == r){
+            l++;
+            r--;
+        }
+        if (l < right){
+            quick(arr, l, right);
+        }
+        if (left < r){
+            quick(arr, left, r);
+        }
+    }
 
 }
--- test/src/leetcode/两数之和.java
+++ test/src/leetcode/两数之和.java
@@ -1,7 +1,41 @@
-package leetcode;/**
+package leetcode;
+
+/**
  * @Author: ruan
  * Date: 2021/11/15 20:21
- * @Description:
-*/
+ * @Description: 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ */
 public class 两数之和 {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] ints = twoSum1(nums, target);
+        for (int i : ints){
+            System.out.println(i);
+        }
+
+    }
+
+    /**
+     * 解法1，双指针暴力循环
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum1(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
 }
