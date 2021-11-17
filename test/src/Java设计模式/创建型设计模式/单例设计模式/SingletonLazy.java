32d4bacf28d032cd4af4b72bdd25a87b39f3bfba
ruan-luqingnian-r <15991875018@163.com>
2021-11-17 14:57:09 +0800

    'test'

--- test/src/Java设计模式/创建型设计模式/单例设计模式/SingletonLazy.java
+++ test/src/Java设计模式/创建型设计模式/单例设计模式/SingletonLazy.java
@@ -1,12 +1,14 @@
 package Java设计模式.创建型设计模式.单例设计模式;
 
+import java1031.LazySingleton;
+
 /**
  * @Author: ruan
  * Date: 2021/11/17 14:29
  * @Description: 单例，懒汉式
  */
 public class SingletonLazy {
-    private static SingletonLazy instance;
+    private static volatile SingletonLazy instance;
     /**
      * 私有化构造方法
      */
@@ -31,13 +33,40 @@ public class SingletonLazy {
     }*/
 
     /**
+     * 第二种方法
      * 通过synchronized加锁保证单例
      * 采用synchronized对方法加锁有很大的性能开销
+     * 解决方法：降低锁粒度
      * @return
      */
-    public synchronized static SingletonLazy getInstance(){
+    /*public synchronized static SingletonLazy getInstance(){
         if (instance == null){
             instance = new SingletonLazy();
+        }
+        return instance;
+    }*/
+
+    /**
+     * 第三种方法
+     * DCL双重检查锁定（Double-Checked-Locking），多线程情况下保证高性能
+     * 是否安全？
+     * instance = new SingletonLazy()并不是原子性操作？
+     * 1、分配空间给对象
+     * 2、在空间内创建对象
+     * 3、对象赋值给引用
+     * 所以要加上volatile关键字，防止指令重排序
+     * @return
+     */
+    public static SingletonLazy getInstance(){
+        //第一重检查
+        if (instance == null){
+            //线程锁定
+            synchronized (LazySingleton.class){
+                //第二重检查
+                if (instance == null){
+                    instance = new SingletonLazy();
+                }
+            }
         }
         return instance;
     }
