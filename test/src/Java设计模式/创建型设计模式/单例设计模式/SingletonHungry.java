810c679c0f349ffbadf9fc9424db529a70c537d3
ruan-luqingnian-r <15991875018@163.com>
2021-11-17 15:17:24 +0800

    'test'

---
+++ test/src/Java设计模式/创建型设计模式/单例设计模式/SingletonHungry.java
@@ -0,0 +1,14 @@
+package Java设计模式.创建型设计模式.单例设计模式;
+
+/**
+ * @Author: ruan
+ * Date: 2021/11/17 15:12
+ * @Description: 单例设计模式-饿汉式
+ */
+public class SingletonHungry {
+    private static SingletonHungry instance = new SingletonHungry();
+    private SingletonHungry (){};
+    public SingletonHungry getInstance(){
+        return instance;
+    }
+}
