62ebfaec165a6fd6a5cad01579a183d2dfc59a9d
ruan-luqingnian-r <15991875018@163.com>
2021-11-20 13:02:49 +0800

    'test'

---
+++ test/src/leetcode/寻找两个正序数组的中位数.java
@@ -0,0 +1,75 @@
+package leetcode;
+
+/**
+ * @Author: ruan
+ * Date: 2021/11/19 11:39
+ * @Description: 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ *
+ * 算法的时间复杂度应该为 O(log (m+n))
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 寻找两个正序数组的中位数 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,3};
+        int[] nums2 = {2};
+        double medianSortedArrays = findMedianSortedArrays(nums1, nums2);
+        System.out.println(medianSortedArrays);
+
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums;
+        int m = nums1.length;
+        int n = nums2.length;
+        nums = new int[m + n];
+        if (m == 0) {
+            if (n % 2 == 0) {
+                return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
+            } else {
+
+                return nums2[n / 2];
+            }
+        }
+        if (n == 0) {
+            if (m % 2 == 0) {
+                return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
+            } else {
+                return nums1[m / 2];
+            }
+        }
+
+        int count = 0;
+        int i = 0, j = 0;
+        while (count != (m + n)) {
+            if (i == m) {
+                while (j != n) {
+                    nums[count++] = nums2[j++];
+                }
+                break;
+            }
+            if (j == n) {
+                while (i != m) {
+                    nums[count++] = nums1[i++];
+                }
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                nums[count++] = nums1[i++];
+            } else {
+                nums[count++] = nums2[j++];
+            }
+        }
+
+        if (count % 2 == 0) {
+            return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+        } else {
+            return nums[count / 2];
+        }
+
+    }
+}
