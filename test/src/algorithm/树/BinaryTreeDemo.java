e680ed5ed7273944724052047c4605141cc56a07
ruan-luqingnian-r <15991875018@163.com>
2021-11-21 16:06:24 +0800

    'test'

--- test/src/algorithm/树/BinaryTreeDemo.java
+++ test/src/algorithm/树/BinaryTreeDemo.java
@@ -14,10 +14,12 @@ public class BinaryTreeDemo {
         HeroNode node2 = new HeroNode(2, "吴用");
         HeroNode node3 = new HeroNode(3, "卢俊义");
         HeroNode node4 = new HeroNode(4, "林冲");
+        HeroNode node5 = new HeroNode(5, "关胜");
         //先手动创建二叉树，后期递归创建二叉树
         node1.setLeft(node2);
         node1.setRight(node3);
         node3.setRight(node4);
+        node3.setLeft(node5);
         binaryTree.setRoot(node1);
         System.out.println("==前序==");
         binaryTree.preOrder();
